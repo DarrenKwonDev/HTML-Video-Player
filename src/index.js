0ac33212e24b9d817849e4bc86c259522b5d22e1
DarrenKwonDev <anatomy1545@gmail.com>
2020-07-13 19:57:56 +0900

    show duration, current time

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import "./styles.css";
+import "./styles.scss";
+import "regenerator-runtime/runtime";
 
 const videoPlayer = document.querySelector(".videoPlayer");
 const playButton = document.querySelector(".playButton");
 const volumeButton = document.querySelector(".volumeButton");
+const currentTime = document.querySelector(".currentTime");
+const totalTime = document.querySelector(".totalTime");
 
 const handlePlayButtonClick = () => {
   if (videoPlayer.paused) {
@@ -26,9 +29,39 @@ const handleVolumnButtonClcik = () => {
   }
 };
 
+const formatDate = (seconds) => {
+  const secondsNumber = parseInt(seconds, 10);
+  let hours = Math.floor(secondsNumber / 3600);
+  let minutes = Math.floor((secondsNumber - hours * 3600) / 60);
+  let totalSeconds = secondsNumber - hours * 3600 - minutes * 60;
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (totalSeconds < 10) {
+    totalSeconds = `0${totalSeconds}`;
+  }
+  return `${hours}:${minutes}:${totalSeconds}`;
+};
+
+function getCurrentTime() {
+  const currentTimeString = formatDate(Math.floor(videoPlayer.currentTime));
+  currentTime.innerHTML = currentTimeString;
+}
+
+async function setTotalTime() {
+  const totalTimeString = formatDate(Math.floor(videoPlayer.duration));
+  totalTime.innerHTML = totalTimeString;
+  setInterval(getCurrentTime, 1000);
+}
+
 function init() {
   playButton.addEventListener("click", handlePlayButtonClick);
   volumeButton.addEventListener("click", handleVolumnButtonClcik);
+  videoPlayer.addEventListener("loadeddata", setTotalTime);
 }
 
 init();
